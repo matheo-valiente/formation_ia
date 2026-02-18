0ec3e10b871a6cac180dc536f119d38c4774543b
Augustin <augustin.polette@efrei.net>
2026-02-18 22:07:37 +0100

    facebooklink

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import s from './App.module.css';
 const STRIPE = 'https://buy.stripe.com/7sY14oa8Y9pZ4N26MUcjS00';
 const INSTA = 'https://www.instagram.com/intelligence_artificielle_info';
 const LINKEDIN = 'https://www.linkedin.com/in/augustinp/';
+const FACEBOOK = 'https://www.facebook.com/profile.php?id=61588001016498';
+const TIKTOK = 'https://www.tiktok.com/@intelligence_artificielle_info';
 
 
 function useReveal(threshold = 0.12) {
@@ -723,7 +725,7 @@ export default function App() {
                   <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-2.88 2.5 2.89 2.89 0 0 1-2.89-2.89 2.89 2.89 0 0 1 2.89-2.89c.28 0 .54.04.79.1v-3.5a6.37 6.37 0 0 0-.79-.05A6.34 6.34 0 0 0 3.15 15a6.34 6.34 0 0 0 6.34 6.34 6.34 6.34 0 0 0 6.34-6.34V8.87a8.16 8.16 0 0 0 4.76 1.52v-3.4a4.85 4.85 0 0 1-1-.3z"/>
                 </svg>
               </a>
-              <a href="#" className={s.footerSocial} target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+              <a href={FACEBOOK} className={s.footerSocial} target="_blank" rel="noopener noreferrer" aria-label="Facebook">
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
                   <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
                 </svg>
